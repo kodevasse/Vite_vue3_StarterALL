5d27b5b5dde352961c8a75df395bed6aea3626b5
kodevasse <kodevasse@outlook.com>
2022-01-10 12:36:15 +0100

    trying use head SEO

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,23 +33,10 @@ const notes = computed(() => store.state.notes);
       </li>
     </ul>
     <div
-      class="
-        max-w-screen-xl
-        px-4
-        py-12
-        mx-auto
-        sm:px-6
-        lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between
-      "
+      class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between"
     >
       <h2
-        class="
-          text-3xl
-          font-extrabold
-          leading-9
-          tracking-tight
-          sm:text-4xl sm:leading-10
-        "
+        class="text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10"
       >
         About Page
         <br />
@@ -70,3 +57,17 @@ const notes = computed(() => store.state.notes);
     </div>
   </div>
 </template>
+
+<script setup>
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "Website Title",
+  meta: [
+    {
+      name: `description`,
+      content: `Website description`,
+    },
+  ],
+});
+</script>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,3 +5,28 @@
     </div>
   </header>
 </template>
+
+<script>
+import { defineComponent, computed, reactive } from "vue";
+import { useHead } from "@vueuse/head";
+
+export default defineComponent({
+  setup() {
+    const siteData = reactive({
+      title: `My website`,
+      description: `My beautiful website`,
+    });
+
+    useHead({
+      // Can be static or computed
+      title: computed(() => siteData.title),
+      meta: [
+        {
+          name: `description`,
+          content: computed(() => siteData.description),
+        },
+      ],
+    });
+  },
+});
+</script>
